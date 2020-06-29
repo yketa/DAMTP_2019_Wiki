4562be0d2c597ddd1a52bbb70541407f7b519574
Yann-Edwin Keta <yann-edwin.keta@ens-lyon.fr>
2020-06-29 16:30:31 +0200

    plot scripts RTP

---
+++ Summaries/RTPring/exact_plot.py
@@ -0,0 +1,83 @@
+from active_work.miscellaneous import RTPring
+from active_work.plot import list_colormap
+from active_work.init import get_env
+
+import matplotlib.pyplot as plt
+plt.style.use('paper')
+from matplotlib.lines import Line2D
+
+import numpy as np
+
+r = RTPring()
+
+_L = [.5, 1, 2, 5, 10]
+
+x = {L: np.array(np.linspace(-1./L, 0, 500).tolist() + np.linspace(0, 2, 500).tolist()) for L in _L}
+s = {L: np.array(list(map(lambda _: r.s(L, _), x[L])))*L for L in _L}
+nu = {L: np.array(list(map(lambda _: r.nu(L, _), x[L]))) for L in _L}
+
+smin = -10
+xmax = 0.5
+
+# plot
+
+colors = list_colormap(_L, sort=True)
+adjust = {'left': 0.28, 'right': 0.98}
+
+# psi
+
+fig, ax = plt.subplots()
+ax.set_xlabel(r'$\tilde{\lambda} \tilde{L}$')
+ax.set_xlim([-10, 10])
+ax.set_xticks([-10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10])
+ax.set_xticklabels(['', r'$-8$', '', r'$-4$', '', r'$0$', '', r'$4$', '', r'$8$', ''])
+ax.set_ylabel(r'$\tilde{\psi}^{\rm RTP}(\tilde{\lambda})$')
+
+line = {}
+for L in _L:
+	line[L], = ax.plot(s[L][(x[L] < xmax)*(s[L] > smin)], x[L][(x[L] < xmax)*(s[L] > smin)],
+		color=colors[L], label=r'$\tilde{L} = %s$' % L)
+ax.axvline(x=0, color='black', linewidth=2)
+ax.axhline(y=0, color='black', linewidth=2)
+
+plt.sca(ax)
+ax.add_artist(plt.legend(loc='lower right', ncol=1, borderpad=0.2,
+	handles=[
+		Line2D([0], [0], lw=0, label=r'$\tilde{L}=$'),
+		*[Line2D([0], [0], color=line[L].get_color(), label=r'$%s$' % L)
+			for L in _L]]))
+fig.subplots_adjust(**adjust)
+
+# nu
+
+fign, axn = plt.subplots()
+axn.set_xlabel(r'$\tilde{\lambda} \tilde{L}$')
+axn.set_xlim([-10, 10])
+axn.set_xticks([-10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10])
+axn.set_xticklabels(['', r'$-8$', '', r'$-4$', '', r'$0$', '', r'$4$', '', r'$8$', ''])
+axn.set_ylabel(r'$\nu(\tilde{\lambda})$')
+
+linen = {}
+for L in _L:
+#	linen[L], = axn.plot(s[L][(x[L] < xmax)*(s[L] > smin)], nu[L][(x[L] < xmax)*(s[L] > smin)],
+	linen[L], = axn.plot(s[L][(s[L] > smin)], nu[L][(s[L] > smin)],
+		color=colors[L], label=r'$\tilde{L} = %s$' % L) 
+
+plt.sca(axn)
+axn.add_artist(plt.legend(loc='upper right', ncol=1, borderpad=0.2,
+	handles=[
+		*[Line2D([0], [0], color=linen[L].get_color(), label=r'$\tilde{L}=%s$' % L)
+			for L in _L]]))
+fign.subplots_adjust(**adjust)
+
+# show and save
+
+if get_env('SAVE', default=False, vartype=bool):
+	def save(f, fname):
+		f.savefig(fname + '.eps')
+		f.savefig(fname + '.svg')
+	save(fig, 'exactPsiRTP')
+	save(fign, 'exactNuRTP')
+
+if get_env('SHOW', default=True, vartype=bool): plt.show()
+
---
+++ Summaries/RTPring/scaling_regime_plot.py
@@ -0,0 +1,82 @@
+from active_work.init import get_env
+from active_work.plot import list_colormap
+from active_work.miscellaneous import RTPring
+r = RTPring()
+
+import matplotlib.pyplot as plt
+plt.style.use('paper')
+
+import numpy as np
+
+Lambda = np.linspace(-50, 2, 100)
+
+# Psi
+
+figL, axL = plt.subplots()
+axL.set_xlabel(r'$\Lambda$')
+axL.set_xlim([-50, 10])
+axL.set_xticks([-50, -40, -30, -20, -10, 0, 10])
+axL.set_xticklabels(['', r'$-40$', '', r'$-20$', '', r'$0$', ''])
+axL.set_ylabel(r'$\Psi(\Lambda)$')
+axL.set_ylim([-np.pi**2/4, np.pi**2/8])
+axL.set_yticks([-np.pi**2/4, -3*np.pi**2/16, -np.pi**2/8, -np.pi**2/16, 0, np.pi**2/16, np.pi**2/8])
+axL.set_yticklabels([r'$-\frac{\pi^2}{4}$', r'$-\frac{3 \pi^2}{16}$', r'$-\frac{\pi^2}{8}$', r'$-\frac{\pi^2}{16}$', r'$0$', r'$\frac{\pi^2}{16}$', r'$\frac{\pi^2}{8}$'])
+
+axL.plot(Lambda, list(map(r.Psi, Lambda)))
+
+axL.axhline(y=0, color='black', linestyle='-', lw=2)
+axL.axvline(x=0, color='black', linestyle='-', lw=2)
+
+adjust = {'left': 0.25, 'top': 0.92, 'right': 0.99}
+figL.subplots_adjust(**adjust)
+
+# Gamma
+
+figG, axG = plt.subplots()
+axG.set_xlabel(r'$\Lambda$')
+axG.set_xlim([-50, 10])
+axG.set_xticks([-50, -40, -30, -20, -10, 0, 10]) 
+axG.set_xticklabels(['', r'$-40$', '', r'$-20$', '', r'$0$', ''])
+axG.set_ylabel(r'$\Psi(\Lambda)/\Lambda$')
+axG.set_ylim([0.0, 1.6])
+
+axG.plot(Lambda, list(map(r.Gamma, Lambda)))
+
+axG.axhline(y=1, color='black', linestyle='-', lw=2)
+axG.axvline(x=0, color='black', linestyle='-', lw=2)
+
+figG.subplots_adjust(**adjust)
+
+# epsilon
+
+fige, axe = plt.subplots()
+axe.set_xlabel(r'$r/L$')
+axe.set_ylabel(r'$L \varepsilon_{\Lambda}(r)$')
+
+x = np.linspace(0, 1, 100)
+_Lambda = [-3, -2, -1, 0, 1, 2]
+colors = list_colormap(_Lambda, sort=True)
+for l in sorted(_Lambda):
+	axe.plot(x, r.LEpsilon(l, *x), color=colors[l],
+		label=r'$\Lambda=%i$' % l)
+
+plt.sca(axe)
+plt.legend(loc='upper center', ncol=3)
+
+fige.set_size_inches(11.25, 5)
+fige.subplots_adjust(left=0.11, top=0.93, right=0.97)
+
+# show/save
+
+if get_env('SAVE', default=False, vartype=bool):
+
+	def save(f, fname):
+		f.savefig(fname + '.eps')
+		f.savefig(fname + '.svg')
+
+	save(figL, 'lambdaRTP')
+	save(figG, 'gammaRTP')
+	save(fige, 'epsilonRTP')
+
+if get_env('SHOW', default=True, vartype=bool): plt.show()
+
